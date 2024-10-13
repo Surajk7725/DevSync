3b5f2cd8e4c9d8101cc7b035268dcfd7d83eac2b
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-10-13 22:49:12 +0530

    Completed Project

---
+++ devsync/src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import UserInfo from './Components/UserInfo';
+import Repos from './Components/Repos';
+import  GlobalStyle  from './styles/GlobalStyle';
+import AppStyles from './styles/AppStyles';
+import InputStyles from './styles/InputStyles';
+import  ButtonStyles from './styles/ButtonStyles';
+
+const App = () => {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [repoData, setRepoData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const userResponse = await axios.get(`https://api.github.com/users/${username}`);
+      const reposResponse = await axios.get(`https://api.github.com/users/${username}/repos`);
+      setUserData(userResponse.data);
+      setRepoData(reposResponse.data);
+    } catch (error) {
+      console.error('Error fetching data from GitHub', error);
+      setUserData(null);
+      setRepoData([]);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    fetchData();
+  };
+
+  return (
+    <AppStyles>  {/* This code gives about the background color of whole website */}
+      <GlobalStyle /> {/* This code gives about the global css of whole website */}
+      <h1 style={{color: "white"}}>Type Your GitHub Username</h1>
+      <InputStyles
+        type="text"
+        placeholder="Enter GitHub Username"
+        value={username}
+        onChange={handleInputChange}
+      />
+      <ButtonStyles onClick={handleButtonClick}>Search</ButtonStyles>
+      {userData && <UserInfo userData={userData} />}
+      {repoData.length > 0 && <Repos repoData={repoData} />}
+      </AppStyles>
+  );
+};
+
+export default App;
